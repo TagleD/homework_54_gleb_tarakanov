5748ee352c3fd2cbc8e2bace8670c0524a1b731b
GLeb Tarakanov <glebtgd@gmail.com>
2023-02-18 21:37:53 +0600

    Добавлен шаблон category.html Добавлена страница, вьюшка и т.д. добавления категории продукта

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from webapp.views.categories import category_add
 from webapp.views.products import products_view, product_view
 
 urlpatterns = [
     path('', products_view, name='index'),
     path('products', products_view, name='products_view'),
-    path('products/<int:pk>', product_view, name='product_detail')
+    path('products/<int:pk>', product_view, name='product_detail'),
+    path('categories/add', category_add, name='category_add')
 ]
